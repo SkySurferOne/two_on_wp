03a5c2c8cae0715ba0f94e0181017e9f392aa3b0
SkySurferOne <skysurfer95@interia.pl>
2015-05-27 18:07:39 +0200

    config.rb fix

--- wp-content/themes/two_on_wp/config.rb
+++ wp-content/themes/two_on_wp/config.rb
@@ -8,6 +8,9 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+line_comments = false
+sourcemap = true
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
